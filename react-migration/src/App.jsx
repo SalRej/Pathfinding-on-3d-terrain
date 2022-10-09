d79005de8e7d8e4368c8450bd3304c1f518e09a6
SalRej <sal3wwm@gmail.com>
2022-10-09 18:52:06 +0300

    changed variables to useRef to keep vlaue on rerender

--- react-migration/src/App.jsx
+++ react-migration/src/App.jsx
@@ -5,26 +5,31 @@ import initScene from '../scripts/initScene';
 import createNoiseMap from '../scripts/createNoiseMap';
 import djikstra from '../scripts/djikstra';
 import animatePathFinding from '../scripts/animatePathFinding';
-
 function App() {
 
-  let scene,camera,renderer,controls;
-  let pathData = [];
-  let vizualizingMesh;
-  let pathMesh;
+  const scene = useRef();
+  const camera = useRef ();
+  const renderer = useRef();
+  const controls = useRef();
+  const pathData = useRef([]);
+  const vizualizingMesh = useRef();
+  const pathMesh = useRef();
 
-  const drawRanges = {
+  const drawRanges = useRef({
     vizualizeMeshDrawRange:0,
     pathDrawRange:0
-  }
-  let worldData;
+  })
+
+  const worldData = useRef();
+
+  const [ counter , setCounter ] = useState(0);
 
   useEffect(()=>{
     const initObjects = initScene();
-    scene = initObjects.scene;
-    camera = initObjects.camera;
-    renderer= initObjects.renderer;
-    controls = initObjects.controls;
+    scene.current = initObjects.scene;
+    camera.current = initObjects.camera;
+    renderer.current = initObjects.renderer;
+    controls.current = initObjects.controls;
 
     // gui for map generation control
     const scale = 70;
@@ -32,61 +37,63 @@ function App() {
     const persistance = 0.5;
     const lacunarity = 2;
 
-    worldData = createNoiseMap(100,100,scale,octaves,persistance,lacunarity,scene);
+    worldData.current = createNoiseMap(130,130,scale,octaves,persistance,lacunarity,scene.current);
 
     animate();
 
   },[]);
 
   const findPath = () => {
-    pathData= djikstra(worldData.graph,0,4300);
-    drawRanges.vizualizeMeshDrawRange = 0;
-    drawRanges.pathDrawRange = 0;
+    pathData.current = djikstra(worldData.current.graph,0,4300);
+
+    drawRanges.current.vizualizeMeshDrawRange = 0;
+    drawRanges.current.pathDrawRange = 0;
 
     const vizualizingMeshGeometry = new THREE.BufferGeometry();
-    vizualizingMeshGeometry.setAttribute( 'position', new THREE.Float32BufferAttribute( pathData.vizualizingGeometry, 3 ));
+    vizualizingMeshGeometry.setAttribute( 'position', new THREE.Float32BufferAttribute( pathData.current.vizualizingGeometry, 3 ));
     vizualizingMeshGeometry.computeVertexNormals();//needed for light to work
     
     const vizualizingMeshMaterial = new THREE.MeshStandardMaterial( {
       side: THREE.DoubleSide,color:0xffffff,opacity:0.4,transparent:true
     });
 
-    vizualizingMesh = new THREE.Mesh( vizualizingMeshGeometry, vizualizingMeshMaterial );
-    vizualizingMesh.name = "vizualizingMesh";
-    
-    scene.remove(scene.getObjectByName("pathMesh"));
+    vizualizingMesh.current = new THREE.Mesh( vizualizingMeshGeometry, vizualizingMeshMaterial );
+    vizualizingMesh.current.name = "vizualizingMesh";
+    scene.current.remove(scene.current.getObjectByName("pathMesh"));
 
     const pathGeometry = new THREE.BufferGeometry();
-    pathGeometry.setAttribute( 'position', new THREE.Float32BufferAttribute( pathData.path, 3 ));
+    pathGeometry.setAttribute( 'position', new THREE.Float32BufferAttribute( pathData.current.path, 3 ));
     pathGeometry.computeVertexNormals();//needed for light to work
     
     const pathMaterial = new THREE.MeshStandardMaterial( {
       side: THREE.DoubleSide,color:0xff0000
     });
 
-    pathMesh = new THREE.Mesh( pathGeometry, pathMaterial );
-    pathMesh.name = "pathMesh";
+    pathMesh.current = new THREE.Mesh( pathGeometry, pathMaterial );
+    pathMesh.current.name = "pathMesh";
 
-    pathMesh.geometry.setDrawRange(0,drawRanges.pathDrawRange);
-    scene.add(vizualizingMesh);
-    scene.add(pathMesh);
+    pathMesh.current.geometry.setDrawRange(0,drawRanges.pathDrawRange);
+
+    scene.current.add(vizualizingMesh.current);
+    scene.current.add(pathMesh.current);
   }
 
   function animate() {
     requestAnimationFrame( animate );
-    controls.update();
-    renderer.render( scene, camera );
+    controls.current.update();
+    renderer.current.render( scene.current, camera.current );
   
     // vizualize the pathfinding
-    if(pathData.vizualizingGeometry!=undefined){
-      animatePathFinding(vizualizingMesh,pathMesh,drawRanges,scene);
+    if(pathData.current.vizualizingGeometry!=undefined){
+      animatePathFinding(vizualizingMesh.current,pathMesh.current,drawRanges.current,scene.current);
     }
-  
   };
 
   return (
     <div className="App" id="App">
         <button onClick={findPath}>click</button>
+        <button onClick={ ()=>{setCounter((prev)=>prev+1)} }>Increment</button>
+        <p>{counter}</p>
     </div>
   )
 }
