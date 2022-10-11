04f7fe0e51013b3c51028eb307df8edf481f1d70
SalRej <sal3wwm@gmail.com>
2022-10-11 16:10:26 +0300

    separated noise generation controls into component

--- react-migration/src/App.jsx
+++ react-migration/src/App.jsx
@@ -6,6 +6,7 @@ import createNoiseMap from '../scripts/createNoiseMap';
 import djikstra from '../scripts/djikstra';
 import animatePathFinding from '../scripts/animatePathFinding';
 import createHeightMap from '../scripts/createHightMap';
+import NoiseGeneratorControls from './NoiseGeneratorControls';
 
 function App() {
 
@@ -50,8 +51,8 @@ function App() {
 
     const gridHelper = new THREE.GridHelper( size, divisions );
     scene.current.add( gridHelper );
-    // worldData.current = createNoiseMap(generationVariables,scene.current,true);
-    createHeightMap(200,200,scene.current);
+    worldData.current = createNoiseMap(generationVariables,scene.current,true);
+    // createHeightMap(200,200,scene.current);
     animate();
   },[]);
 
@@ -122,28 +123,10 @@ function App() {
   return (
     <div className="App" id="App">
         <button onClick={findPath}>click</button>
-        <button onClick={ ()=>{setCounter((prev)=>prev+1)} }>Increment</button>
-
-        <form onChange={handleGenerationVariableChange}>
-          
-          <p>Scale:{generationVariables.scale}</p>
-          <input type='range' min={0} max={200} name="scale" value={generationVariables.scale}></input>
-          <p>Octaves:{generationVariables.octaves}</p>
-          <input type='range' min={0} max={10} name="octaves" value={generationVariables.octaves}></input>
-          <p>Lacunarity:{generationVariables.lacunarity}</p>
-          <input type='range' min={0} max={5} name="lacunarity" value={generationVariables.lacunarity}></input>
-          <p>Persistance:{generationVariables.persistance}</p>
-          <input type='range' min={0} max={2} name="persistance" value={generationVariables.persistance}></input>
-
-          <p>offsetX:{generationVariables.offsetX}</p>
-          <input type='range' min={0} max={5} name="offsetX" step={0.1} value={generationVariables.offsetX}></input>
-          <p>offsetY:{generationVariables.offsetY}</p>
-          <input type='range' min={0} max={5} name="offsetY" step={0.1} value={generationVariables.offsetY}></input>
-          
-          <p>Seed</p>
-          <input type='text' name="seed" value={generationVariables.seed}></input>
-
-        </form>
+        <NoiseGeneratorControls 
+          generationVariables={generationVariables}
+          handleGenerationVariableChange={handleGenerationVariableChange}
+        />
     </div>
   )
 }
