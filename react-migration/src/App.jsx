ed9d30c738e574e0c709b55e6b83fce11a7bb44b
SalRej <sal3wwm@gmail.com>
2022-10-18 10:36:17 +0300

    created contex provider with shared data

--- react-migration/src/App.jsx
+++ react-migration/src/App.jsx
@@ -1,24 +1,45 @@
-import React from 'react';
+import React , { useState , useEffect, useRef}from 'react';
 import { Routes , Route } from "react-router-dom";
 
 import Home from './Home';
 import NoiseGeneration from './NoiseGeneration';
 import HeightMapGeneration from './HeightMapGeneration';
 
-function App() {
+import initScene from '../scripts/initScene';
+import worldDataContext from './contex';
 
-  // const findPath = () => {
-  //   pathData.current = djikstra(worldData.current.graph,3000,25300);
-  // }
+function App(){
+  
+  const THREEScene = useRef({
+    camera:null,
+    scene:null,
+    renderer:null,
+    controls:null,
+  })
+  const [pathFindingVariables,setPathFindingVariables] = useState({
+    startId:-1,
+    endId:-1,
+    isEnagled:false,
+    graph:[]
+  })
 
+  useEffect(()=>{
+    const initObjects = initScene();
+    THREEScene.current.camera=initObjects.camera;
+    THREEScene.current.renderer=initObjects.renderer;
+    THREEScene.current.controls=initObjects.controls;
+    THREEScene.current.scene=initObjects.scene;
+  },[])
 
   return (
     <div className="App" id="App">
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path='/noiseGeneration' element={<NoiseGeneration/>}/>
-        <Route path='/heightMapGeneration' element={<HeightMapGeneration/>}/>
-      </Routes>
+      <worldDataContext.Provider value={{THREEScene, pathFindingVariables}}>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+            <Route path='/noiseGeneration' element={<NoiseGeneration/>}/>
+            <Route path='/heightMapGeneration' element={<HeightMapGeneration/>}/>
+        </Routes>
+      </worldDataContext.Provider>
     </div>
   )
 }
--- react-migration/src/HeightMapSettings.jsx
+++ react-migration/src/HeightMapSettings.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React , {useState} from 'react'
 import Slider from './Slider';
-function HeightMapSettings({heightMapVariables , handleHeightMapSettings , loadImage , changeResolution}) {
+import PathSettings from './PathSettings';
+function HeightMapSettings({heightMapVariables , handleHeightMapSettings , loadImage , changeResolution , pathFindingVariables , setIsPathfindingEnabled}) {
 
   const handleChange = (event) =>{
     handleHeightMapSettings(event);
@@ -14,19 +15,37 @@ function HeightMapSettings({heightMapVariables , handleHeightMapSettings , loadI
     const numberValue = Number(event.target.value)
     changeResolution(numberValue);
   }
+  const [chosenTab , setChosenTab] = useState("settings");
+
   return (
     <div className='controls'>
       <p className='heading'>World generation settings</p>
+      <nav>
+          <p className={chosenTab==="settings"?"chosen":""} onClick={()=>setChosenTab("settings")}>Generation</p>
+          <p className={chosenTab==="path"?"chosen":""} onClick={()=>setChosenTab("path")}>Find path</p>
+      </nav>
 
-      <form onChange={handleResolutionChange}>
-        <Slider min={0} max={500} step={1} name={"resolution"} value={heightMapVariables.numPointsX}/>
-      </form>
+      {
+        chosenTab==="settings" &&
+        <main>
+          <form onChange={handleResolutionChange}>
+            <Slider min={0} max={500} step={1} name={"resolution"} value={heightMapVariables.numPointsX}/>
+          </form>
 
-      <form onChange={handleChange}>
-        <Slider min={0} max={50} step={1} name={"scaleY"} value={heightMapVariables.scaleY}/>
-        <p>Choose an image</p>
-        <input type='file' accept="image/png, image/jpeg" onChange={handleFileInput}></input>
-      </form>
+          <form onChange={handleChange}>
+            <Slider min={0} max={50} step={1} name={"scaleY"} value={heightMapVariables.scaleY}/>
+            <p>Choose an image</p>
+            <input type='file' accept="image/png, image/jpeg" onChange={handleFileInput}></input>
+          </form>
+        </main>
+      }
+      {
+        chosenTab==="path" &&
+        <PathSettings 
+            pathFindingVariables={pathFindingVariables}
+            setIsPathfindingEnabled={setIsPathfindingEnabled}
+        />
+      }
     </div>
   )
 }
