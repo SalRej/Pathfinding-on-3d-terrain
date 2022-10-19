87d6d63a9d41514bba3c8b3d852dc8a1c7fbafa6
SalRej <sal3wwm@gmail.com>
2022-10-19 13:03:11 +0300

    changed the way colors is aplied

--- react-migration/scripts/applyColor.js
+++ react-migration/scripts/applyColor.js
@@ -1,89 +1,54 @@
 import mapping from "./mapping";
-const setColor = (color,r,g,b)=>{
-    color.r=r;
-    color.g=g;
-    color.b=b;
+const setColor = (color1,color2)=>{
+    color1.r=color2.r;
+    color1.g=color2.g;
+    color1.b=color2.b;
 }
 
 const applyColor = (y1,y2,y3,color1,color2,color3) => {
 
-    setColor(color1,0,0.3,0.6);
-    setColor(color2,0,0.3,0.6);
-    setColor(color3,0,0.3,0.6);
+    setColor(color1,{r:0,g:0.3,b:0.6});
+    setColor(color2,{r:0,g:0.3,b:0.6});
+    setColor(color3,{r:0,g:0.3,b:0.6});
 
-    //yellow
-    if(y1>0.15){
-        setColor(color1,1,0.8,0);
-    }
-    if(y2>0.15){
-        setColor(color2,1,0.8,0);
+    const colorsAndValues =[
+        {
+            value:0.15,
+            color:{r:1,g:0.8,b:0}//yellow
+        },
+        {
+            value:0.225,
+            color:{r:0.3,g:0.9,b:0.3}//light green
+        },
+        {
+            value:0.4,
+            color:{r:0.3,g:0.7,b:0.3}//darker green
+        },
+        {
+            value:0.6,
+            color:{r:0.5,g:0.2,b:0.1}//lighter brown
+        },
+        {
+            value:0.75,
+            color:{r:0.3,g:0.19,b:0.05}//darker brown
+        },
+        {
+            value:0.9,
+            color:{r:1,g:1,b:1}
+        }
+    ]
 
-    }
-    if(y3>0.15){
-        setColor(color3,1,0.8,0);
-    }
+    for(let i = 0;i<colorsAndValues.length;i++){
 
-    //lightGreen
-    if(y1>0.225){
-        setColor(color1,0.3,0.9,0.3);
-    }
-    if(y2>0.225){
-        setColor(color2,0.3,0.9,0.3);
-
-    }
-    if(y3>0.225){
-        setColor(color3,0.3,0.9,0.3);
-    }
-
-    //dark green
-    if(y1>0.4){
-        setColor(color1,0.3,0.7,0.3);
-    }
-    if(y2>0.4){
-        setColor(color2,0.3,0.7,0.3);
-
-    }
-    if(y3>0.4){
-        setColor(color3,0.3,0.7,0.3);
-    }
-
-    //light brown
-    if(y1>0.6){
-        setColor(color1,0.5,0.2,0.1);
-    }
-    if(y2>0.6){
-        setColor(color2,0.5,0.2,0.1);
-
-    }
-    if(y3>0.6){
-        setColor(color3,0.5,0.2,0.1);
-
-    }
-
-    //dark brown
-    if(y1>0.75){
-        setColor(color1,0.3,0.19,0.05);
-    }
-    if(y2>0.75){
-        setColor(color2,0.3,0.19,0.05);
-
-    }
-    if(y3>0.75){
-        setColor(color3,0.3,0.19,0.05);
-
-    }
-
-    //white
-    if(y1>0.9){
-        setColor(color1,1,1,1);
-
-    }
-    if(y2>0.9){
-        setColor(color2,1,1,1);
-
-    }
-    if(y3>0.9){
-        setColor(color3,1,1,1);
+        if(y1>colorsAndValues[i].value){
+            setColor(color1,colorsAndValues[i].color);
+        }
+        if(y2>colorsAndValues[i].value){
+            setColor(color2,colorsAndValues[i].color);
+        }
+        if(y3>colorsAndValues[i].value){
+            setColor(color3,colorsAndValues[i].color);
+        }
     }
 }
 export default applyColor;
