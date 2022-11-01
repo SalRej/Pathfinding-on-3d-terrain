688df0e46e52dc8a5ce3802679f6835ddba419f7
SalRej <sal3wwm@gmail.com>
2022-11-01 19:35:54 +0200

    moved the skybox folder into public

--- code/scripts/animate.js
+++
@@ -1,7 +0,0 @@
-function animate(THREEScene) {
-    requestAnimationFrame( animate);
-    // console.log(THREEScene);
-    THREEScene.controls.update();
-    THREEScene.renderer.render( THREEScene.scene, THREEScene.camera);
-};
-export default animate;
