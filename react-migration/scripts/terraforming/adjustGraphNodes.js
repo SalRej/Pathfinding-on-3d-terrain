5e54bcd1cfd2549c8cf42d579232207741c50e1f
SalRej <sal3wwm@gmail.com>
2022-11-01 16:32:06 +0200

    flooring the avrageY to 2 digits so it dosent cause problems

--- react-migration/scripts/terraforming/adjustGraphNodes.js
+++ react-migration/scripts/terraforming/adjustGraphNodes.js
@@ -3,13 +3,13 @@ import mapping from "../mapping";
 
 const adjustGraphNodes = (nodesIdsToAdjust,graph,ind,positions,scaleY) =>{
     for(let i = 0;i<nodesIdsToAdjust.length;i++){
-
+        
         const id = nodesIdsToAdjust[i];
-
+        
         const point1 = ind.getX(id*3);
         const point2 = ind.getY(id*3);
         const point3 = ind.getZ(id*3);
-  
+        
         const y1 = positions.getY(point1);
         const y2 = positions.getY(point2);
         const y3 = positions.getY(point3);
@@ -17,8 +17,9 @@ const adjustGraphNodes = (nodesIdsToAdjust,graph,ind,positions,scaleY) =>{
         const normalizedY1 = mapping(y1,0,scaleY,0,1);
         const normalizedY2 = mapping(y2,0,scaleY,0,1);
         const normalizedY3 = mapping(y3,0,scaleY,0,1);
-
-        const avrageY = (normalizedY1+normalizedY2+normalizedY3)/3;//needed to determine cost value of each node
+        
+        let avrageY = (normalizedY1+normalizedY2+normalizedY3)/3;//needed to determine cost value of each node
+        avrageY=avrageY.toFixed(2);
         const movingCost = calculateMovingCost(avrageY);
 
         graph[id].cost = movingCost;
