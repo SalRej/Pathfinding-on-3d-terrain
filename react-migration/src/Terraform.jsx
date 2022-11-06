6947180822a1b84954305020c282f98d0e26c2fe
SalRej <sal3wwm@gmail.com>
2022-11-06 23:30:22 +0200

    added info block to terraform component

--- react-migration/src/Terraform.jsx
+++ react-migration/src/Terraform.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 function Terraform() {
   return (
-    <main>Terraform</main>
+    <main>
+        <div className='info'>
+        <img src="icons8-info-24.png"></img>
+            <ul>
+                <li>camera will be locked durring terrafoming</li>
+                <li>left button to rise terrain</li>
+                <li>right button to lower the terrain</li>
+            </ul>
+        </div>
+        <button>Enable Terraforming</button>
+    </main>
   )
 }
 
