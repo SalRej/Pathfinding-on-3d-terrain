722b5fa83ab9088b30e1027548205cdb0145de43
SalRej <sal3wwm@gmail.com>
2022-11-06 23:30:16 +0200

    added styles to pathfinding settings

--- react-migration/src/PathSettings.jsx
+++ react-migration/src/PathSettings.jsx
@@ -7,10 +7,25 @@ function PathSettings({pathFindingVariables, setIsPathfindingEnabled}){
     }
     return (
         <main>
-            <span>Start: {pathFindingVariables.startId===-1?"none":pathFindingVariables.startId}</span>
-            <span>End: {pathFindingVariables.endId===-1?"none":pathFindingVariables.endId}</span>
+            <div className='start-end'>
+                <p>Start: {pathFindingVariables.startId===-1?"none":pathFindingVariables.startId}</p>
+                <p>End: {pathFindingVariables.endId===-1?"none":pathFindingVariables.endId}</p>
+            </div>
 
-            <button onClick={handleClick}>{pathFindingVariables.isEnagled===false?"Enable Pathfinding":"Disable Pathfinding"}</button>
+            <div className='info'>
+                <img src="icons8-info-24.png"></img>
+                <ul>
+                    <li>Press left mouse button to place a start point</li>
+                    <li>Press right mouse button to place an end point</li>
+                    <li>Start and end points must be selected</li>  
+                    <li>When working with high resolution of map , pahtfinding my take a few seconds</li>
+                </ul>
+            </div>
+
+            <button onClick={handleClick}
+                className={pathFindingVariables.isEnagled===true?"enabled":"disabled"}>
+                {pathFindingVariables.isEnagled===false?"Enable Pathfinding":"Disable Pathfinding"}
+            </button>
         </main>
     )
 }
