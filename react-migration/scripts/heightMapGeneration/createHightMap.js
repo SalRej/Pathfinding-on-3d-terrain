f9ca673a47401e6d2b891baab7d93e8c6a09010a
SalRej <sal3wwm@gmail.com>
2022-11-06 23:30:24 +0200

    terraforming now works with heightmap

--- react-migration/scripts/heightMapGeneration/createHightMap.js
+++ react-migration/scripts/heightMapGeneration/createHightMap.js
@@ -2,59 +2,8 @@ import * as THREE from 'three'
 import { DoubleSide } from 'three';
 import applyColor from '../applyColor';
 import mapping from '../mapping';
-import createPoints from '../createPoints';
-import createNode from '../graph/createNode';
-
-const createMesh = (points , pointsOfTriangleIndexes , scaleY ,width) =>{
-
-    const trianglePositions = [];
-    const bufferGeometryColors = [];
-    const graph = [];
-    for(let i = 0;i < pointsOfTriangleIndexes.length ; i++){
-
-        const x1 = points[pointsOfTriangleIndexes[i].a].x;
-        const x2 = points[pointsOfTriangleIndexes[i].b].x;
-        const x3 = points[pointsOfTriangleIndexes[i].c].x;
-
-        let y1 = points[pointsOfTriangleIndexes[i].a].y;
-        let y2 = points[pointsOfTriangleIndexes[i].b].y;
-        let y3 = points[pointsOfTriangleIndexes[i].c].y;
-
-        const z1 = points[pointsOfTriangleIndexes[i].a].z;
-        const z2 = points[pointsOfTriangleIndexes[i].b].z;
-        const z3 = points[pointsOfTriangleIndexes[i].c].z;
-
-        
-        trianglePositions.push(
-            x1,mapping(y1,0,1,0,scaleY),z1,
-            x2,mapping(y2,0,1,0,scaleY),z2,
-            x3,mapping(y3,0,1,0,scaleY),z3
-        )
-
-        const color1={r:0,g:0,b:0};
-        const color2={r:0,g:0,b:0};
-        const color3={r:0,g:0,b:0};
-            
-        // console.log(y1,y2,y3);
-        applyColor(y1,y2,y3,color1,color2,color3);
-        bufferGeometryColors.push(
-            color1.r,color1.g,color1.b,
-            color2.r,color2.g,color2.b,
-            color3.r,color3.g,color3.b
-        )
-        const position = [x1,mapping(y1,0,1,0,scaleY)+1,z1,x2,mapping(y2,0,1,0,scaleY)+1,z2,x3,mapping(y3,0,1,0,scaleY)+1,z3];
-        const avrageY = (y1+y2+y3)/3;//needet to determine cost value of each node
-        createNode(graph,i,avrageY,width,position);
-    }
-
-    
-    const bufferGeometry = new THREE.BufferGeometry();
-    bufferGeometry.setAttribute( 'position', new THREE.Float32BufferAttribute( trianglePositions, 3 ));
-    bufferGeometry.computeVertexNormals();
-    bufferGeometry.setAttribute('color', new THREE.Float32BufferAttribute( bufferGeometryColors, 3 ));
-
-    return {bufferGeometry,graph};
-}
+import createPoints from './createPoints';
+import createGraph from '../graph/createGraph';
 
 const createHeightMap = (heightMapVariables,scene) =>{
 
@@ -74,8 +23,8 @@ const createHeightMap = (heightMapVariables,scene) =>{
     //get pixels from canvas
     const pixelsOfHeightMapImg = ctx.getImageData(0,0,numPointsX,numPointsY);
     const { points , pointsOfTriangleIndexes } = createPoints(numPointsX,numPointsY);
-    
-    let j = 0;
+    const colors = [];
+    let j = 1;
     for(let i = 0; i < pixelsOfHeightMapImg.data.length ; i+=4){
         const color = {r:0,g:0,b:0};
         color.r = pixelsOfHeightMapImg.data[i];
@@ -83,21 +32,31 @@ const createHeightMap = (heightMapVariables,scene) =>{
         color.b = pixelsOfHeightMapImg.data[i+2];
         
         const grey = (color.r + color.b + color.g)/3;
-    
-        points[j].y = mapping(grey,0,255,0,1);
-        j++;
+        
+        const normalizedY = mapping(grey,0,255,0,1);
+        points[j] = mapping(normalizedY,0,1,0,scaleY);
+
+        const colorToApply = {r:0,g:0,b:0};
+        applyColor(normalizedY,colorToApply);
+        colors.push(colorToApply.r,colorToApply.g,colorToApply.b);
+        j+=3;
+
     }
-    
-    const {bufferGeometry,graph} = createMesh(points,pointsOfTriangleIndexes,scaleY,numPointsX);
+    const bufferGeometry = new THREE.BufferGeometry();
+    bufferGeometry.setIndex(pointsOfTriangleIndexes);
+    bufferGeometry.setAttribute( 'position', new THREE.Float32BufferAttribute( points, 3 ));
+    bufferGeometry.computeVertexNormals();
+    bufferGeometry.setAttribute('color', new THREE.Float32BufferAttribute( colors, 3 ));
+
     const material = new THREE.MeshStandardMaterial({
         side:DoubleSide,
         vertexColors:true
     });
-        
+    
+    const graph = createGraph(bufferGeometry,numPointsX);
     const mesh = new THREE.Mesh(bufferGeometry,material);
     mesh.name="worldMesh";
     scene.add(mesh); 
-    canvas.remove();
     return graph; 
 }
 
--- react-migration/src/NoiseGeneratorSettings.jsx
+++ react-migration/src/NoiseGeneratorSettings.jsx
@@ -21,8 +21,8 @@ function NoiseGeneratorControls({generationVariables , handleNoiseSettings , cha
             <nav>
                 <p className={chosenTab==="settings"?"chosen":""} onClick={()=>setChosenTab("settings")}>Generation</p>
                 <p className={chosenTab==="path"?"chosen":""} onClick={()=>setChosenTab("path")}>Find path</p>
-                <p className={chosenTab==="export"?"chosen":""} onClick={()=>setChosenTab("export")}>Export</p>
                 <p className={chosenTab==="terraform"?"chosen":""} onClick={()=>setChosenTab("terraform")}>Terraform</p>
+                <p className={chosenTab==="export"?"chosen":""} onClick={()=>setChosenTab("export")}>Export</p>
             </nav>
             
             {   chosenTab==="settings" &&
