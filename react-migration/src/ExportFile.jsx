7bab2c052f469501b77b7db685744bd9cfaff395
SalRej <sal3wwm@gmail.com>
2022-11-06 23:30:19 +0200

    now can download a glb or gltf file

--- react-migration/src/ExportFile.jsx
+++ react-migration/src/ExportFile.jsx
@@ -1,13 +1,57 @@
-import React from 'react'
+import React , {useContext , useRef , useEffect} from 'react'
+import worldDataContext from './contex';
+import { GLTFExporter } from 'three/addons/exporters/GLTFExporter.js';
 
 function ExportFile() {
-  return (
-    <main>
-        <button>Export now
-            <img src='download.png'></img>
-        </button>
-    </main>
-  )
+
+    const {THREEScene} = useContext(worldDataContext);
+    const downloadLink = useRef(null);
+    function save( blob, filename ) {
+
+        if(downloadLink.current!=null){
+            downloadLink.current.href = URL.createObjectURL( blob );
+            downloadLink.current.download = filename;
+            downloadLink.current.click();
+        }
+    }
+
+    function saveString( text, filename ) {
+
+        save( new Blob( [ text ], { type: 'text/plain' } ), filename );
+
+    }
+
+    const download = () =>{
+        const {scene} = THREEScene;
+        const gltfExporter = new GLTFExporter();
+        const input = scene.getObjectByName('worldMesh');
+
+        gltfExporter.parse(
+            input,
+            function ( result ) {
+                if ( result instanceof ArrayBuffer ) {
+                    saveArrayBuffer( result, 'scene.glb' );
+                } else {
+                    const output = JSON.stringify( result, null, 2 );
+                    console.log( output );
+                    saveString( output, 'scene.gltf' );
+                }
+            },
+            function ( error ) {
+                console.log( 'An error happened during parsing', error );
+            }
+        )
+    }
+    
+    return (
+        <main>
+            <a ref={downloadLink} >
+                <button onClick={download}>Export now
+                    <img src='download.png'></img>
+                </button>
+            </a>
+        </main>
+    )
 }
 
 export default ExportFile
