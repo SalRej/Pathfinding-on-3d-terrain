3da12689a16da46c392ecd5c9710a9790e670c72
SalRej <sal3wwm@gmail.com>
2022-11-06 23:30:25 +0200

    added hook to hanled button hold when terraforming

---
+++ react-migration/src/hooks/useOnButtonHold.jsx
@@ -0,0 +1,41 @@
+import {useRef} from 'react'
+import terraform from '../../scripts/terraforming/terraform';
+import getTriangleClicked from '../../scripts/getTriangleClicked';
+
+function useOnButtonHold(THREEScene,terraformingVariables,pathFindingVariables,mouseX,mouseY,scaleY){
+
+    const intervalRef = useRef(null);
+    const startCounter = (event) => {
+
+        if (intervalRef.current) return;
+        if(terraformingVariables.isEnabled===false) return;
+
+        intervalRef.current = setInterval(() => {
+
+          const {renderer,scene,camera} = THREEScene;
+          const triangleId = getTriangleClicked(mouseX.current,mouseY.current,renderer,camera,scene);
+
+          if(event.button===0){//left button
+            terraform(triangleId,THREEScene,pathFindingVariables,scaleY,true,terraformingVariables);
+          }
+          else if(event.button===2){//right button
+            terraform(triangleId,THREEScene,pathFindingVariables,scaleY,false,terraformingVariables);
+          }
+          
+        }, 10);
+    };
+    
+    const stopCounter = () => {
+        if (intervalRef.current) {
+            clearInterval(intervalRef.current);
+            intervalRef.current = null;
+        }
+    };
+
+    return{
+        startCounter,
+        stopCounter
+    }
+}
+
+export default useOnButtonHold
